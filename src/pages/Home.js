5cb6957bc6c2c5390224e91ad3bca5aee40fbeb3
SamirAjdini <ajdinis1993@gmail.com>
2022-07-02 18:18:19 +0200

    implemented hero component

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroComponent from "../components/hero/HeroComponent";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <HeroComponent />
+    </div>
+  );
+};
+
+export default Home;
