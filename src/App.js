3c5c830c7b46c270698f34eefe089ef127c3b51c
SamirAjdini <ajdinis1993@gmail.com>
2022-07-16 22:16:48 +0200

    added suspense and loading spinner

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, { useContext } from "react";
+import React, { Suspense, useContext } from "react";
 import "./App.css";
 import Navigation from "./components/nav/Navigation";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Excercises from "./pages/Excercises";
-import Excercise from "./pages/Excercise";
-
 import HttpContext from "./context/http-context";
+import LoadingSpinner from "./components/home-components/loading-spinner/LoadingSpinner";
 function App() {
   const isFetched = useContext(HttpContext).isFetched;
-  console.log(isFetched);
+
+  const Home = React.lazy(() => import("./pages/Home"));
+  const Excercises = React.lazy(() => import("./pages/Excercises"));
+  const Excercise = React.lazy(() => import("./pages/Excercise"));
+
   return (
     <>
       <Navigation>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {isFetched && (
-            <Route path="/list/:excerciseList" element={<Excercises />} />
-          )}
-          {isFetched && <Route path="/excercise/:id" element={<Excercise />} />}
-        </Routes>
+        <Suspense
+          fallback={
+            <div className="center">
+              <LoadingSpinner asOverlay />
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {isFetched && (
+              <Route path="/list/:excerciseList" element={<Excercises />} />
+            )}
+            {isFetched && (
+              <Route path="/excercise/:id" element={<Excercise />} />
+            )}
+          </Routes>
+        </Suspense>
       </Navigation>
     </>
   );
